77b9671e96548c57b0a643f9935eff2227dcb18d
Deep <deepgauravraj@gmail.com>
2020-06-19 09:46:44 +0530

    remove not send

--- src/downloader_trait.rs
+++ src/downloader_trait.rs
@@ -3,7 +3,7 @@ use async_trait::async_trait;
 use failure::Error;
 use std::collections::HashMap;
 
-#[async_trait(?Send)]
+#[async_trait]
 pub trait Downloader {
     async fn download(url: &str) -> Result<String, ParsingError>;
     async fn download_with_header(
